f32011fae1c4e32781f921cbfb8e90a8c50c5307
Sean Mayer <2008sean@live.com>
2021-11-17 06:51:58 +0000

    Init Crowd Funding Smart Contract
    - Added Read me (Contract Requirements)
    - Added CrowdFunding contract with initial variables, constructor and payable and view functions

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Crowd-Funding-Smart-Contract
+#Crowd Funding Contract
+
+- Admin starts campaign
+- Admin has can set a sepcific monetary goal and deadline
+- Contributors will be able tp send ETH to particpate 
+- Spending requests are required so that the admin can use the funds
+- Contributors can vote on the spending request
+- If more than 50% of Contributors voted for that request, then the admin will have permission to spedn the amount set in the spending request
+- The power is moved from the campaign admin to those who donated the money
+- Contributors can request a refund if monetary goal is not reached within the deadline
---
+++ contracts/crowdfunding.sol
@@ -0,0 +1,43 @@
+//SPDX-License-Identifier: GPL-3.0 
+
+pragma solidity >=0.5 <0.9.0;
+
+
+contract CrowdFunding {
+    mapping(address => uint) public contributors;
+    address public admin;
+    uint public noOfContributors;
+    uint public minimumContribution;
+    uint public deadline; //timestamp
+    uint public goal;
+    uint public raisedAmount;
+    
+    constructor(uint _goal, uint _deadline) {
+        goal = _goal;
+        deadline = block.timestamp + _deadline;
+        minimumContribution = 100 wei;
+        admin = msg.sender;
+    }
+    
+    function contribute() public payable {
+        require(block.timestamp < deadline, "Deadline has passed!");
+        require(msg.value >= minimumContribution, "Minimum Contribution not met!");
+        
+        if(contributors[msg.sender] == 0) {
+            noOfContributors++;
+        }
+        
+        contributors[msg.sender] += msg.value;
+        raisedAmount += msg.value;
+        
+    }
+    
+    receive() payable external {
+        contribute();
+        
+    }
+    
+    function getBalance() public view returns(uint) {
+        return address(this).balance;
+    }
+}
