fcfe34061256819816dcd71c979707a9e593ef8a
Sean Mayer <2008sean@live.com>
2021-11-18 06:44:15 +0000

    Added Spending Request
    - createRequest and voteRequests functions have been created
    - struct has been used to encapsulate a requests characteristics

--- contracts/crowdfunding.sol
+++ contracts/crowdfunding.sol
@@ -4,6 +4,7 @@ pragma solidity >=0.5 <0.9.0;
 
 
 contract CrowdFunding {
+    
     mapping(address => uint) public contributors;
     address public admin;
     uint public noOfContributors;
@@ -11,6 +12,19 @@ contract CrowdFunding {
     uint public deadline; //timestamp
     uint public goal;
     uint public raisedAmount;
+    
+    struct Request {
+        string description;
+        address payable recipient;
+        uint value;
+        bool completed;
+        uint noOfVoters;
+        mapping(address => bool) voters;
+    }
+    
+    mapping(uint => Request) public requests;
+    
+    uint public numRequests;
     
     constructor(uint _goal, uint _deadline) {
         goal = _goal;
@@ -39,5 +53,41 @@ contract CrowdFunding {
     
     function getBalance() public view returns(uint) {
         return address(this).balance;
+    }
+    
+    function getRefund() public {
+        require(block.timestamp > deadline && raisedAmount < goal);
+        require(contributors[msg.sender] > 0);
+        
+        address payable recipient = payable(msg.sender);
+        uint value = contributors[msg.sender];
+        recipient.transfer(value);
+        
+        contributors[msg.sender] = 0;
+    }
+    
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "Only admin can call this function!");
+        _;
+    }
+    
+    function createRequest(string memory _description, address payable _recipient, uint _value) public onlyAdmin {
+        Request storage newRequest = requests[numRequests];
+        numRequests++;
+        
+        newRequest.description = _description;
+        newRequest.recipient = _recipient;
+        newRequest.value = _value;
+        newRequest.completed = false;
+        newRequest.noOfVoters = 0;
+    }
+    
+    function voteRequest(uint _requestNo) public {
+        require(contributors[msg.sender] > 0,"You must be a contributor to vote!");
+        Request storage thisRequest = requests[_requestNo];
+        
+        require(thisRequest.voters[msg.sender] == false, "You have already voted!");
+        thisRequest.voters[msg.sender] = true;
+        thisRequest.noOfVoters++;
     }
 }
